7ff4a231272a97b0c514ecc75e73399d565d5ce1
qtdemo <linminglu@163.com>
2013-12-11 17:39:21 +0800

    add

--- src/emryshttplib/httplibtt.go
+++ src/emryshttplib/httplibtt.go
@@ -7,13 +7,13 @@ import (
 
 func T() {
 	fmt.Print("test function")
-	resp, err := Get("http://beego.me").Debug(true).getResponse()
+	resp, err := Get("http://www.baidu.com").Debug(true).getResponse()
 	data, err := ioutil.ReadAll(resp.Body)
 	defer resp.Body.Close()
 	if err != nil {
 		fmt.Print("hello\n\n")
 	}
-	fmt.Print(data)
+	fmt.Print(string(data))
 
 	//fmt.Print(data)
 }
